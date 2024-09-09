2c94b5c679cb371f70aca4200e508eb411a5c948
bbyik-k <kby00517@naver.com>
2024-09-09 23:11:19 +0900

    Add local storage

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ TODO App with react, 2024 ver
 
 ## GOAL
 
-1. TODO APP
+1. TODO APP ✅
 2. 전체 아이템 리스트 보여주기 ✅
 3. 아이템 삭제 ✅
 4. 아이템 체크박스 ✅
 5. 아이템 추가 ✅
 6. 아이템 필터링 ✅
-7. 다크모드 지원
-8. 로컬스토리지 저장
+7. 다크모드 지원 ✅
+8. 로컬스토리지 저장 ✅
 9. 아이템 이름 수정
 10. 날짜별 분리
 11. 날짜 검색
@@ -26,13 +26,13 @@ TODO App with react, 2024 ver
 ## Dev Log
 
 - 2024-08-30(금)
-  - 1h 39m 작업
+  - 1h 30m 작업
   - 기본 뼈대 -> Todo list 구성 완료
 - 2024-08-31(토)
-  - 43m 작업
+  - 40m 작업
   - todo item prop 전달, 아이템 삭제 delete 기능 간단 구현 완료
 - 2024-09-01(일)
-  - 34m 작업
+  - 30m 작업
   - Add Todo 기본 기능 구현
   - Todo list data 및 handling 함수 메인 컴포넌트 AppTodo.jsx 로 이동
 - 2024-09-02(월)
@@ -44,7 +44,15 @@ TODO App with react, 2024 ver
   - 40m 작업
   - filter style 추가
   - dark mode theme 기본 기능 구현
+- 2024-09-06(금)
+  - 30m 작업
+  - Check 기능 함수 추가
+- 2024-09-09(월)
+  - 40m 작업
+  - local storage set & get 기능 추가
+  - 기본 기능 전체 구현 완료!
 
 ---
 
-- ~2024-09-04(일) 현재 total 작업시간: 약 5h
+- ~2024-09-09(월) 현재 total 작업시간: 약 5h 50m
+  - 기본 기능 전체 구현완!
--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -6,29 +6,49 @@ import Header from './components/Header/Header';
 import { ThemeProvider } from './context/ThemeContext';
 
 export default function AppTodo_() {
+  const initialTodo = JSON.parse(localStorage.getItem('todoList') || '[]');
   const [todoList, setTodo] = useState(initialTodo);
+
+  console.log(initialTodo);
+
   // const [todoState, setTodoState] = useState('active');
   const [todoState, setTodoState] = useState('all');
   // const [todoState, setTodoState] = useState('completed');
 
-  const handleDelete = (id) => {
-    setTodo((prev) => {
-      return prev.filter((todo) => todo.id !== id);
-    });
-  };
-  const handleAdd = (name) => {
-    setTodo((prev) => {
-      return [...prev, { id: Date.now().toString(), name, completed: false }];
-    });
+  const setLocal = (arr) => {
+    // console.log('setLocal');
+    // console.log(todoList);
+    if (!arr) return;
+    localStorage.setItem('todoList', JSON.stringify(arr));
   };
 
   const handleState = (state) => {
     setTodoState(state);
   };
 
+  const handleDelete = (id) => {
+    setTodo((prev) => {
+      const delArr = prev.filter((todo) => todo.id !== id);
+
+      setLocal(delArr);
+      return delArr;
+    });
+  };
+  const handleAdd = (name) => {
+    setTodo((prev) => {
+      const addArr = [...prev, { id: Date.now().toString(), name, completed: false }];
+
+      setLocal(addArr);
+      return addArr;
+    });
+  };
+
   const handleCheck = (id) => {
     setTodo((prev) => {
-      return prev.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+      const checkArr = prev.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+
+      setLocal(checkArr);
+      return checkArr;
     });
   };
 
@@ -43,8 +63,13 @@ export default function AppTodo_() {
   );
 }
 
-const initialTodo = [
+const initialTodo_ = [
   { id: '1', name: '운동', completed: true },
   { id: '2', name: '공부', completed: false },
   { id: '3', name: '야마', completed: true },
 ];
+
+// console.log(initialTodo_);
+
+const initStr = initialTodo_.toString();
+// console.log(JSON.parse(initStr));
